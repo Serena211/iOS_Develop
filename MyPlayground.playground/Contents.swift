fc1e6d89741402cdee820c0ba55291224b494243
Xuanyu Liu <sunflowerxuanyu@gmail.com>
2016-07-02 15:11:43 -0400

    first time using playground

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,20 @@
+//: Playground - noun: a place where people can play
+
+// import a framework, which is code that is proviede to you to use
+// a UIKit emcompasses all of the UI stuff that you need to build an iOS app and to use very basic stuff, like strings, and images, etc.
+import UIKit
+
+// Define variables: VAR or LET
+var s:String? = nil     // ? may not be String
+s = "Hey"
+
+var str:String = "Hello, playground"
+
+var num = 3
+
+print(num)
+
+var image = UIImage(named: "tomcat.png")
+
+// let is safer, and if something's not supposed to change, define it as a constant
+let constant = "Cannot be changed!!!"
