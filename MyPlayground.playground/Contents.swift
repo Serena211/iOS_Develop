0c5342d541175e1ba5da572ac5b43303e3bc3806
Xuanyu Liu <sunflowerxuanyu@gmail.com>
2016-07-02 19:11:43 -0400

    more practice

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -18,3 +18,44 @@ var image = UIImage(named: "tomcat.png")
 
 // let is safer, and if something's not supposed to change, define it as a constant
 let constant = "Cannot be changed!!!"
+
+// Control flow
+
+for number in [2,3,5,6,0] {
+    number * number
+}
+
+// Arrays and Dictionaries
+var animals:[String] = ["Cow","Dog","Bunny"]     // define an array
+//animals[0] = "Cat"
+
+for animal in animals {
+    animal
+}
+
+var cuteness = ["Cow" : "Not Very",
+                "Dog" : "Cute",
+                "Bunny" : "Very Cute"]
+
+cuteness["Dog"]
+
+for animal in animals {
+    cuteness[animal]
+}
+
+// Function
+func doMath(operation: String, on a: Double, and b: Double ) -> Double {
+    print("Doing Math")
+    print("Performing", operation, "on", a, "and",b)
+    var rsl:Double = 0
+    switch operation {
+        case "+": rsl = a + b
+        case "-": rsl = a - b
+        case "*": rsl = a * b
+        case "/": rsl = a / b
+    default: print("Bad operation", operation)
+    }
+    return rsl
+}
+let result = doMath("+", on: 1.0, and: 2.0)
+
